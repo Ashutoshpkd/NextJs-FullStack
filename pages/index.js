fe152dbe21c54f9ebbe2867a95dea0bd07f9dd6b
Ashutosh Pawade <ashpawad@WKMZTE2DB86B.global.publicisgroupe.net>
2023-01-25 09:31:20 +0530

    Initial routing and page setup

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import { Fragment } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+
+const meetups = [
+    {
+        id: 'm1',
+        image: 'https://i.pinimg.com/originals/54/67/4a/54674a83dc8ea08d2641bee45b9d2ba3.jpg',
+        title: 'Its snowy here, Lets catch up',
+        address: 'North Road, Gibsen Street, 2241',
+    },
+    {
+        id: 'm2',
+        image: 'https://i.pinimg.com/originals/54/67/4a/54674a83dc8ea08d2641bee45b9d2ba3.jpg',
+        title: 'Meet in the beach house',
+        address: 'North Road, Gibsen Street, 2241',
+    },
+    {
+        id: 'm3',
+        image: 'https://i.pinimg.com/originals/54/67/4a/54674a83dc8ea08d2641bee45b9d2ba3.jpg',
+        title: 'Its covered in green',
+        address: 'North Road, Gibsen Street, 2241',
+    }
+];
+const HomePage = () => {
+    return (
+        <Fragment>
+            <MeetupList meetups={meetups}/>
+        </Fragment>
+    );
+};
+
+export default HomePage;
